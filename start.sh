4cdebb038f1852e374fc8ab654f00e1559734906
Zack YL Shih <zackyl.shih@moxa.com>
2014-11-18 11:22:31 +0800

    Now support BUNDLE_PATH, BUNDLE_MAIN to run docker

--- start.sh
+++ start.sh
@@ -8,10 +8,11 @@ echo "BUNDLE_PATH: $BUNDLE_PATH"
 echo "BUNDLE_MAIN: $BUNDLE_MAIN"
 echo "Working Directory: $FIG_PATH"
 
-cp Dockerfile fig.yml $FIG_PATH
+cp .dockerignore Dockerfile fig.yml $FIG_PATH
 cp -r $BUNDLE_PATH $FIG_PATH/bundle
 
 sed -i "s/\$FIG_PATH/"$(echo $FIG_PATH | sed -e 's/[\/&]/\\&/g')"/g" $FIG_PATH/fig.yml
 sed -i "s/\$BUNDLE_PATH/"$(echo $BUNDLE_PATH | sed -e 's/[\/&]/\\&/g')"/g" $FIG_PATH/fig.yml
+sed -i "s/\$BUNDLE_MAIN/"$(echo $BUNDLE_MAIN | sed -e 's/[\/&]/\\&/g')"/g" $FIG_PATH/fig.yml
 
 fig -p bundledev -f $FIG_PATH/fig.yml up
